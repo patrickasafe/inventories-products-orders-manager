8791073a8991b545c78613395716fbd594be1af4
patrickasafe <patrickasafe@gmail.com>
2022-05-27 11:44:30 -0300

    EDIT fix EnchancedTable keys

--- frontend/src/components/EnchancedTable/index.tsx
+++ frontend/src/components/EnchancedTable/index.tsx
@@ -118,7 +118,7 @@ export default function EnhancedTable( untreatedData ) {
                       role="checkbox"
                       aria-checked={isItemSelected}
                       tabIndex={-1}
-                      key={row.name}
+                      key={row.id}
                       selected={isItemSelected}
                     >
                       <TableCell padding="checkbox">
