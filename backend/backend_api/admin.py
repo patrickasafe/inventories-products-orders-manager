9dc929b0d70c0fbc589787ff6a80512b35cab6e4
patrickasafe <patrickasafe@gmail.com>
2022-07-15 20:24:06 -0300

    EDIT backend_api.admin to see new tables at admin django page

--- backend/backend_api/admin.py
+++ backend/backend_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from backend_api.models import Product, Inventory, InventoryProduct, Supplier
+from backend_api.models import Order, OrderProduct, Product, Inventory, InventoryProduct, Supplier
 
 
 class Suppliers(admin.ModelAdmin):
@@ -23,15 +23,28 @@ class Inventories(admin.ModelAdmin):
     list_per_page = 20
 
 
+class Orders(admin.ModelAdmin):
+    list_display = ('id', 'date_order', 'date_shipment')
+    list_display_links = ('id',)
+    search_fields = ('id',)
+    list_per_page = 20
 
 
-class ProductsInventories(admin.ModelAdmin):
+class InventoriesProducts(admin.ModelAdmin):
     list_display = ('inventory', 'product', 'quantity')
-    # list_display_links = ('id',)
+    list_display_links = ('inventory', 'product')
+    list_per_page = 20
+
+
+class OrderProducts(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
+    list_display_links = ('order', 'product')
     list_per_page = 20
 
 
 admin.site.register(Supplier, Suppliers)
 admin.site.register(Product, Products)
 admin.site.register(Inventory, Inventories)
-admin.site.register(InventoryProduct, ProductsInventories)
+admin.site.register(Order, Orders)
+admin.site.register(InventoryProduct, InventoriesProducts)
+admin.site.register(OrderProduct, OrderProducts)
