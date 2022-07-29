ee622c595b42c57279fb3053601f8348bb987baa
patrickasafe <patrickasafe@gmail.com>
2022-07-29 15:56:00 -0300

    EDIT add create supplier test

--- backend/apps/products/tests/test_views.py
+++ backend/apps/products/tests/test_views.py
@@ -1,11 +1,17 @@
 import factory
+import pytest
 import json
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from apps.products.models import Supplier, Product
 
 from apps.products.factories import ProductFactory, SupplierFactory
+
+supplier_create_url = reverse('products:create_supplier')
+supplier_list_url = reverse('products:list_suppliers')
+product_create_url = reverse('products:create_product')
+product_list_url = reverse('products:list_products')
 
 
 class TestSupplier(TestCase):
@@ -16,17 +22,15 @@ class TestSupplier(TestCase):
         self.supplier_01 = self.Factory(id=1)
         self.supplier_02 = self.Factory(id=2)
 
-        self.create_url = reverse('products:create_supplier')
-        self.list_url = reverse('products:list_suppliers')
         self.get_update_delete_url = reverse(
             'products:get_update_delete_supplier', kwargs={'pk': self.supplier_01.id})
 
     def test_list(self):
-        response = self.client.get(self.list_url)
+        response = self.client.get(supplier_list_url)
 
         self.assertContains(response, 1)
         self.assertContains(response, 2)
-        self.assertEquals(Supplier.objects.count(), 2)
+        self.assertEqual(Supplier.objects.count(), 2)
 
     def test_detail(self):
 
@@ -34,23 +38,36 @@ class TestSupplier(TestCase):
         data = json.loads(response.content)
         content = {'id': self.supplier_01.id, "name": self.supplier_01.name, "phone": self.supplier_01.phone,
                    "email": self.supplier_01.email}
-        self.assertEquals(data, content)
+        self.assertEqual(data, content)
 
-    def test_create(self):
+    # TODO: WRITE A CREATE TEST IN PYTEST. I CANNOT TEST INSIDE TESTCASE CLASSE BECAUSE THIS BUG BELOW:
+    # https://code.djangoproject.com/ticket/22431
+    # def test_create(self):
 
-        supplier = content = factory.build(dict, FACTORY_CLASS=SupplierFactory)
-        response = self.client.post(self.create_url, supplier)
-        data = json.loads(response.content)
-        self.assertEquals(response.status_code, 201)
-        self.assertEquals(data, content)
-        self.assertEquals(Supplier.objects.count(), 3)
+    #     supplier = content = factory.build(dict, FACTORY_CLASS=SupplierFactory)
+    #     response = self.client.post(supplier_create_url, supplier)
+    #     data = json.loads(response.content)
+    #     self.assertEqual(response.status_code, 201)
+    #     self.assertEqual(data, content)
+    #     self.assertEqual(Supplier.objects.count(), 3)
 
-    # TODO WRITE A METHOD TO TEST SOFTDELETE
+    # TODO: WRITE A METHOD TO TEST SOFTDELETE
     # def test_delete(self):
 
     #     response = self.client.delete(self.read_update_delete_url)
-    #     self.assertEquals(response.status_code, 204)
-    #     self.assertEquals(Supplier.objects.count(), 1)
+    #     self.assertEqual(response.status_code, 204)
+    #     self.assertEqual(Supplier.objects.count(), 1)
+
+
+@pytest.mark.django_db
+def test_supplier_create():
+    client = Client()
+    supplier = content = factory.build(dict, FACTORY_CLASS=SupplierFactory)
+    response = client.post(supplier_create_url, supplier)
+    data = json.loads(response.content)
+    data.pop('id')
+    assert response.status_code == 201
+    assert data == content
 
 
 class TestProduct(TestCase):
@@ -61,18 +78,15 @@ class TestProduct(TestCase):
         self.supplier_01 = SupplierFactory(id=1)
         self.product_01 = self.Factory(id=1, supplier=self.supplier_01)
         self.product_02 = self.Factory(id=2, supplier=self.supplier_01)
-
-        self.create_url = reverse('products:create_product')
-        self.list_url = reverse('products:list_products')
         self.get_update_delete_url = reverse(
             'products:get_update_delete_product', kwargs={'pk': self.product_01.id})
 
     def test_list(self):
-        response = self.client.get(self.list_url)
+        response = self.client.get(product_list_url)
 
         self.assertContains(response, "RAN")
         self.assertContains(response, "RAN")
-        self.assertEquals(Product.objects.count(), 2)
+        self.assertEqual(Product.objects.count(), 2)
 
     def test_detail(self):
 
@@ -80,14 +94,28 @@ class TestProduct(TestCase):
         data = json.loads(response.content)
         content = {'id': self.product_01.id, "name": self.product_01.name, "ref": self.product_01.ref,
                    "cost": self.product_01.cost, "price": self.product_01.price, "supplier": self.supplier_01.id}
-        self.assertEquals(data, content)
+        self.assertEqual(data, content)
 
-    # # # TODO: FIX TEST -> error: Cannot encode None for key 'deleted_at' as POST data. Did you mean to pass an empty string or omit the value?
+    # TODO: WRITE A CREATE TEST IN PYTEST. I CANNOT TEST INSIDE TESTCASE CLASSE BECAUSE THIS BUG BELOW:
+    # https://code.djangoproject.com/ticket/22431
     # def test_create(self):
 
     #     product = content = factory.build(dict, FACTORY_CLASS=self.Factory)
-    #     response = self.client.post(self.create_url, product)
+    #     response = self.client.post(supplier_create_url, product)
     #     data = json.loads(response.content)
-    #     self.assertEquals(response.status_code, 201)
-    #     self.assertEquals(data, content)
-    #     self.assertEquals(Product.objects.count(), 3)
+    #     self.assertEqual(response.status_code, 201)
+    #     self.assertEqual(data, content)
+    #     self.assertEqual(Product.objects.count(), 3)
+
+
+# @pytest.mark.django_db
+# def test_product_create():
+#     client = Client()
+#     SupplierFactory._get_or_create(Supplier)
+#     product = content = factory.build(dict, FACTORY_CLASS=ProductFactory)
+#     response = client.post(product_create_url, product)
+#     data = json.loads(response.content)
+#     data.pop('id')
+#     # data.supplier = content["supplier"]
+#     assert response.status_code == 201
+#     assert data == content
