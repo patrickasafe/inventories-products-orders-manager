7511ffe1e28183d4050c23c2498823d1a2e47cc3
patrickasafe <patrickasafe@gmail.com>
2022-08-03 22:54:27 -0300

    EDIT /workspace/backend/apps/inventories/urls.py

--- backend/apps/inventories/urls.py
+++ backend/apps/inventories/urls.py
@@ -1,10 +1,25 @@
-from apps.inventories.views import InventoryCreateAPIView, InventoryListAPIView, InventoryRetrieveUpdateAPIView, InventoryMultipleDestroyRequestAPIView
+from apps.inventories.views import InventoryCreateAPIView, InventoryListAPIView, InventoryProductCreateAPIView, InventoryProductListAPIView, InventoryProductMultipleDestroyRequestAPIView, InventoryProductRetrieveUpdateAPIView, InventoryRetrieveUpdateAPIView, InventoryMultipleDestroyRequestAPIView
 from django.urls import path
 
+app_name = "inventories"
 urlpatterns = [
-    path('', InventoryCreateAPIView.as_view()),
-    path('list/', InventoryListAPIView.as_view()),
-    path('<int:pk>', InventoryRetrieveUpdateAPIView.as_view()),
-    path('delete-request/', InventoryMultipleDestroyRequestAPIView.as_view())
+     
+    # Inventory
+    path('', InventoryCreateAPIView.as_view(), name='create_inventory'),
+    path('list/', InventoryListAPIView.as_view(), name='list_inventories'),
+    path('<int:pk>', InventoryRetrieveUpdateAPIView.as_view(),
+         name='get_update_delete_inventory'),
+    path('delete-request/', InventoryMultipleDestroyRequestAPIView.as_view(),
+         name='destroy_multiple_inventories'),
+
+    # InventoryProduct
+    path('', InventoryProductCreateAPIView.as_view(),
+         name='create_inventory_product'),
+    path('list/', InventoryProductListAPIView.as_view(),
+         name='list_inventories_products'),
+    path('<int:pk>', InventoryProductRetrieveUpdateAPIView.as_view(),
+         name='get_update_delete_inventory_product'),
+    path('delete-request/', InventoryProductMultipleDestroyRequestAPIView.as_view(),
+         name='destroy_multiple_inventories_products'),
 
 ]
